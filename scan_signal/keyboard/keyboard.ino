d0c539018d3a6886e1a4c7e58d5f88544be830be
zy13665097090 <11930191@mail.sustech.edu.cn>
2021-05-24 18:55:39 +0800

    300ms air

--- scan_signal/keyboard/keyboard.ino
+++ scan_signal/keyboard/keyboard.ino
@@ -14,7 +14,9 @@ char ssid[] = "HUAWEI";//HUAWEI
 char pswd[] = "123456987";//123456987
 // the setup function runs once when you press reset or power the board
 BlinkerNumber Number3("pulse width");
+BlinkerNumber Number4("pulse int");
 int a = 300;
+int b = 10000,i=0;
 //int RA = D3, RB = D4, RC = D5, RD = D6, INH1 = D7, INH2 = D8;// 5=RA 4=RB 3=RC
 int  RA = 6, RB = 7;
 void setup() {
@@ -32,21 +34,27 @@ void setup() {
 	//BLINKER_DEBUG.stream(Serial);
 	Blinker.begin(auth, ssid, pswd);
 	Blinker.attachData(dataRead);
+// Blinker.attachData(sendint);
 	Blinker.attachHeartbeat(heartbeat);
 	// RV.attach(RV_callback);//ˢ�°�ť    ע��ص�����
-	//Blinker.attachDataStorage(dataStorage);//�����ƺ���
+//	Blinker.attachDataStorage(dataStorage);//�
 }
 void heartbeat() {
 	Number3.print(a); //��loop��û��Ӧ
+	Number4.print(b);
 	//Number4.print(qita_read);
 }
+//void dataStorage()
+//{
+//    Blinker.dataStorage("data1", random(0,120));
+//}
 // the loop function runs over and over again until power down or reset
 void loop() {
 	Blinker.run();
 	Serial.println(a);
-	
+	Serial.println(b);
 	digitalWrite(D6, HIGH);digitalWrite(D7, HIGH);
-	delay(500);
+	delay(b);
 	digitalWrite(D6, LOW);digitalWrite(D7, HIGH);
 	delay(a);  //����pin
 	//digitalWrite(INH1, LOW);digitalWrite(INH2, HIGH);
@@ -57,8 +65,20 @@ void loop() {
 }
 void dataRead(const String& data)//�����
 {
-
-	BLINKER_LOG("Blinker readString: ", data);//dataΪ��������
-	a = data.toFloat();//aΪ�޸ĵ���ֵ
+  
+	BLINKER_LOG("Blinker readString: ", data);//dataΪ��
+    if(i<1) {
+       a = data.toFloat(); 
+//       i=1;
+    } 
+//	 if(i=1){
+//	  b=data.toFloat();
+//   i=0;
+//	  }
 
 }
+
+//void sendint(const String& interval)
+//{ BLINKER_LOG("Blinker readString: ", interval);��ֵ
+//  b=interval.toFloat();
+// }
